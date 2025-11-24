2c3054a9183b66fccb71a667ae423456f98d3359
Shareiar <islamshareiar@gmail.com>
2025-11-24 16:23:15 +0600

    Add GET route for listing comments by post and PUT route for updating comments. Update existing store and destroy routes to new path structure.
    BREAKING CHANGE: API route paths for comment endpoints have been updated, requiring client adjustments.

--- server/routes/Api/Comment/comment.php
+++ server/routes/Api/Comment/comment.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\Api\CommentController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(CommentController::class)->prefix('/post/comments')->name('comments')->group(function () {
-    Route::post('/{post}', 'store')->name('store');
-    Route::delete('/{comment}', 'destroy')->name('destroy');
+Route::controller(CommentController::class)->group(function () {
+    Route::get('/post/{postId}/comments', 'index')->name('index');
+    Route::post('/post/comments/{postId}', 'store')->name('store');
+    Route::put('/post/comments/{comment}', 'update')->name('update'); 
+    Route::delete('/post/comments/{comment}', 'destroy')->name('destroy');
 });
