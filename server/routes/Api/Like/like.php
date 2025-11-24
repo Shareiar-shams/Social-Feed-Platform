cfedfebfead6618475db575d5a3144fe2127b719
Shareiar <islamshareiar@gmail.com>
2025-11-24 06:35:35 +0600

    Add a new GET route to retrieve likes for posts or comments, enabling clients to fetch user lists who reacted to content. This enhances API functionality for displaying like interactions without modifying existing toggle behavior.

--- server/routes/Api/Like/like.php
+++ server/routes/Api/Like/like.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Api\LikeController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/like/{type}/{id}', [LikeController::class, 'toggle']);
+Route::get('/like/{type}/{id}', [LikeController::class, 'getLikes']);
