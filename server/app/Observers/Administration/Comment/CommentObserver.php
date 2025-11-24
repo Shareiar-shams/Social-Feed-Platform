00a1ec036f45197223578c43f886587055179050
Shareiar <islamshareiar@gmail.com>
2025-11-24 19:32:34 +0600

    Add CommentAccessors and CommentMutators traits for custom attribute handling, and introduce CommentObserver to manage model events such as creation, updates, deletions, restoration, and force deletion.

---
+++ server/app/Observers/Administration/Comment/CommentObserver.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Observers\Administration\Comment;
+
+use App\Models\Comment\Comment;
+
+class CommentObserver
+{
+    /**
+     * Handle the Comment "created" event.
+     */
+    public function created(Comment $comment): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Comment "updated" event.
+     */
+    public function updated(Comment $comment): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Comment "deleted" event.
+     */
+    public function deleted(Comment $comment): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Comment "restored" event.
+     */
+    public function restored(Comment $comment): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Comment "force deleted" event.
+     */
+    public function forceDeleted(Comment $comment): void
+    {
+        //
+    }
+}
